DamianSzymanski/tortoisesvn

commit c61da623ee9d6f0b87008cd33fd2d729864245dc

Merged revision(s) r27384, r27396, r27397, r27398, r27399 from trunk: * Make CBrowseFolder::DisableCheckBox2WhenCheckbox1IsEnabled() actually work on Windows Vista and later. * Update to SQLite 3.14.1 * TortoiseMerge might hang with ignore comments enabled  Based on TortoiseGit rev. b37c326ca53189182fc5dca4944379ec94f0f2eb. * Store the copyfrom-revision when a path change is detected, and use that revision when asked for. * update changelog.
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/branches/1.9.x@27400 154be670-7969-47af-9ab0-c6fb378d2295

src/Utils/MiscUI/BrowseFolder.cpp
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2014 - TortoiseSVN
+// Copyright (C) 2003-2014, 2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -21,6 +21,7 @@
 #include "BrowseFolder.h"
 #include "PathUtils.h"
 #include "SmartHandle.h"
+#include "FileDlgEventHandler.h"
 #include <strsafe.h>
 
 BOOL CBrowseFolder::m_bCheck = FALSE;
@@ -34,6 +35,30 @@ TCHAR CBrowseFolder::m_CheckText2[200];
 CString CBrowseFolder::m_sDefaultPath;
 bool CBrowseFolder::m_DisableCheckbox2WhenCheckbox1IsChecked = false;
 
+class BrowseFolderDlgEventHandler : public CFileDlgEventHandler
+{
+public:
+    BrowseFolderDlgEventHandler()
+        : m_DisableCheckbox2WhenCheckbox1IsChecked(false)
+    {
+    }
+
+    bool m_DisableCheckbox2WhenCheckbox1IsChecked;
+
+    STDMETHODIMP OnCheckButtonToggled(IFileDialogCustomize *pfdc,
+                                      DWORD dwIDCtl,
+                                      BOOL bChecked) override
+    {
+        if (m_DisableCheckbox2WhenCheckbox1IsChecked && dwIDCtl == 101)
+        {
+            if (bChecked)
+                pfdc->SetControlState(102, CDCS_VISIBLE | CDCS_INACTIVE);
+            else
+                pfdc->SetControlState(102, CDCS_VISIBLE | CDCS_ENABLED);
+        }
+        return S_OK;
+    }
+};
 
 CBrowseFolder::CBrowseFolder(void)
 :   m_style(0),
@@ -167,9 +192,18 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
         else
             ret = CANCEL;
 
-        pfd->Release();
-    }
-    else
+    // set the default folder
+    CComPtr<IShellItem> psiDefault;
+    if (FAILED(SHCreateItemFromParsingName(m_sDefaultPath, nullptr, IID_PPV_ARGS(&psiDefault))))
+        return CANCEL;
+    if (FAILED(pfd->SetFolder(psiDefault)))
+        return CANCEL;
+
+    CComObjectStackEx<BrowseFolderDlgEventHandler> cbk;
+    cbk.m_DisableCheckbox2WhenCheckbox1IsChecked = m_DisableCheckbox2WhenCheckbox1IsChecked;
+    CComQIPtr<IFileDialogEvents> pEvents = cbk.GetUnknown();
+
+    if (m_CheckText[0] != 0)
     {
         BROWSEINFO browseInfo       = {};
         browseInfo.hwndOwner        = parent;
@@ -182,9 +216,15 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
 
         PCIDLIST_ABSOLUTE itemIDList = SHBrowseForFolder(&browseInfo);
 
-        //is the dialog canceled?
-        if (!itemIDList)
-            ret = CANCEL;
+    DWORD eventsCookie;
+    if (FAILED(pfd->Advise(pEvents, &eventsCookie)))
+        return CANCEL;
+
+    OnOutOfScope(pfd->Unadvise(eventsCookie););
+
+    // Show the open file dialog
+    if (FAILED(pfd->Show(parent)))
+        return CANCEL;
 
         if (ret != CANCEL)
         {

ext/sqlite/shell.c
@@ -2543,13 +2543,22 @@ static FILE *output_file_open(const char *zFile){
 /*
 ** A routine for handling output from sqlite3_trace().
 */
-static void sql_trace_callback(void *pArg, const char *z){
+static int sql_trace_callback(
+  unsigned mType,
+  void *pArg,
+  void *pP,
+  void *pX
+){
   FILE *f = (FILE*)pArg;
+  UNUSED_PARAMETER(mType);
+  UNUSED_PARAMETER(pP);
   if( f ){
+    const char *z = (const char*)pX;
     int i = (int)strlen(z);
     while( i>0 && z[i-1]==';' ){ i--; }
     utf8_printf(f, "%.*s;\n", i, z);
   }
+  return 0;
 }
 
 /*
@@ -2946,10 +2955,10 @@ static int db_int(ShellState *p, const char *zSql){
 /*
 ** Convert a 2-byte or 4-byte big-endian integer into a native integer
 */
-unsigned int get2byteInt(unsigned char *a){
+static unsigned int get2byteInt(unsigned char *a){
   return (a[0]<<8) + a[1];
 }
-unsigned int get4byteInt(unsigned char *a){
+static unsigned int get4byteInt(unsigned char *a){
   return (a[0]<<24) + (a[1]<<16) + (a[2]<<8) + a[3];
 }
 
@@ -4655,9 +4664,9 @@ static int do_meta_command(char *zLine, ShellState *p){
     p->traceOut = output_file_open(azArg[1]);
 #if !defined(SQLITE_OMIT_TRACE) && !defined(SQLITE_OMIT_FLOATING_POINT)
     if( p->traceOut==0 ){
-      sqlite3_trace(p->db, 0, 0);
+      sqlite3_trace_v2(p->db, 0, 0, 0);
     }else{
-      sqlite3_trace(p->db, sql_trace_callback, p->traceOut);
+      sqlite3_trace_v2(p->db, SQLITE_TRACE_STMT, sql_trace_callback,p->traceOut);
     }
 #endif
   }else
@@ -5317,6 +5326,8 @@ int SQLITE_CDECL wmain(int argc, wchar_t **wargv){
       szHeap = integerValue(zSize);
       if( szHeap>0x7fff0000 ) szHeap = 0x7fff0000;
       sqlite3_config(SQLITE_CONFIG_HEAP, malloc((int)szHeap), (int)szHeap, 64);
+#else
+      (void)cmdline_option_value(argc, argv, ++i);
 #endif
     }else if( strcmp(z,"-scratch")==0 ){
       int n, sz;

commit 2d2873533b8484a8b0588c199213b3db8f05edc6

To fix the broken merge in r27400, copy over the whole files of CBrowseFolder.

src/Utils/MiscUI/BrowseFolder.cpp
@@ -1,5 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
+// Copyright (C) 2016 - TortoiseGit
 // Copyright (C) 2003-2014, 2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
@@ -21,19 +22,13 @@
 #include "BrowseFolder.h"
 #include "PathUtils.h"
 #include "SmartHandle.h"
+#include "OnOutOfScope.h"
 #include "FileDlgEventHandler.h"
 #include <strsafe.h>
 
 BOOL CBrowseFolder::m_bCheck = FALSE;
 BOOL CBrowseFolder::m_bCheck2 = FALSE;
-WNDPROC CBrowseFolder::CBProc = NULL;
-HWND CBrowseFolder::checkbox = NULL;
-HWND CBrowseFolder::checkbox2 = NULL;
-HWND CBrowseFolder::ListView = NULL;
-TCHAR CBrowseFolder::m_CheckText[200];
-TCHAR CBrowseFolder::m_CheckText2[200];
 CString CBrowseFolder::m_sDefaultPath;
-bool CBrowseFolder::m_DisableCheckbox2WhenCheckbox1IsChecked = false;
 
 class BrowseFolderDlgEventHandler : public CFileDlgEventHandler
 {
@@ -62,11 +57,8 @@ public:
 
 CBrowseFolder::CBrowseFolder(void)
 :   m_style(0),
-    m_root(NULL)
+    m_DisableCheckbox2WhenCheckbox1IsChecked(false)
 {
-    SecureZeroMemory(m_displayName, sizeof(m_displayName));
-    SecureZeroMemory(m_title, sizeof(m_title));
-    SecureZeroMemory(m_CheckText, sizeof(m_CheckText));
 }
 
 CBrowseFolder::~CBrowseFolder(void)
@@ -87,7 +79,6 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, LPTSTR path, size_t pathl
 }
 CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CString& sDefaultPath /* = CString() */)
 {
-    retVal ret = OK;        //assume OK
     m_sDefaultPath = sDefaultPath;
     if (m_sDefaultPath.IsEmpty() && !path.IsEmpty())
     {
@@ -108,89 +99,23 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
         m_sDefaultPath = path;
     }
 
-    HRESULT hr;
-
     // Create a new common open file dialog
-    IFileOpenDialog* pfd = NULL;
-    hr = CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&pfd));
-    if (SUCCEEDED(hr))
-    {
-        // Set the dialog as a folder picker
-        DWORD dwOptions;
-        if (SUCCEEDED(hr = pfd->GetOptions(&dwOptions)))
-        {
-            hr = pfd->SetOptions(dwOptions | FOS_PICKFOLDERS | FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST);
-        }
+    CComPtr<IFileOpenDialog> pfd;
+    if (FAILED(pfd.CoCreateInstance(CLSID_FileOpenDialog, NULL, CLSCTX_INPROC_SERVER)))
+        return CANCEL;
 
-        // Set a title
-        if (SUCCEEDED(hr))
-        {
-            TCHAR * nl = wcschr(m_title, '\n');
-            if (nl)
-                *nl = 0;
-            pfd->SetTitle(m_title);
-        }
+    // Set the dialog as a folder picker
+    DWORD dwOptions;
+    if (FAILED(pfd->GetOptions(&dwOptions)))
+        return CANCEL;
+    if (FAILED(pfd->SetOptions(dwOptions | FOS_PICKFOLDERS | FOS_FORCEFILESYSTEM | FOS_PATHMUSTEXIST)))
+        return CANCEL;
 
-        // set the default folder
-        if (SUCCEEDED(hr))
-        {
-            IShellItem *psiDefault = 0;
-            hr = SHCreateItemFromParsingName(m_sDefaultPath, NULL, IID_PPV_ARGS(&psiDefault));
-            if (SUCCEEDED(hr))
-            {
-                hr = pfd->SetFolder(psiDefault);
-                psiDefault->Release();
-            }
-        }
-
-        if (m_CheckText[0] != 0)
-        {
-            IFileDialogCustomize* pfdCustomize = 0;
-            hr = pfd->QueryInterface(IID_PPV_ARGS(&pfdCustomize));
-            if (SUCCEEDED(hr))
-            {
-                pfdCustomize->StartVisualGroup(100, L"");
-                pfdCustomize->AddCheckButton(101, m_CheckText, FALSE);
-                if (m_CheckText2[0] != 0)
-                {
-                    pfdCustomize->AddCheckButton(102, m_CheckText2, FALSE);
-                }
-                pfdCustomize->EndVisualGroup();
-                pfdCustomize->Release();
-            }
-        }
-
-        // Show the open file dialog
-        if (SUCCEEDED(hr) && SUCCEEDED(hr = pfd->Show(parent)))
-        {
-            // Get the selection from the user
-            IShellItem* psiResult = NULL;
-            hr = pfd->GetResult(&psiResult);
-            if (SUCCEEDED(hr))
-            {
-                PWSTR pszPath = NULL;
-                hr = psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszPath);
-                if (SUCCEEDED(hr))
-                {
-                    path = pszPath;
-                    CoTaskMemFree(pszPath);
-                }
-                psiResult->Release();
-
-                IFileDialogCustomize* pfdCustomize = 0;
-                hr = pfd->QueryInterface(IID_PPV_ARGS(&pfdCustomize));
-                if (SUCCEEDED(hr))
-                {
-                    pfdCustomize->GetCheckButtonState(101, &m_bCheck);
-                    pfdCustomize->GetCheckButtonState(102, &m_bCheck2);
-                    pfdCustomize->Release();
-                }
-            }
-            else
-                ret = CANCEL;
-        }
-        else
-            ret = CANCEL;
+    // Set a title
+    TCHAR * nl = wcschr(m_title, '\n');
+    if (nl)
+        *nl = 0;
+    pfd->SetTitle(m_title);
 
     // set the default folder
     CComPtr<IShellItem> psiDefault;
@@ -203,18 +128,18 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
     cbk.m_DisableCheckbox2WhenCheckbox1IsChecked = m_DisableCheckbox2WhenCheckbox1IsChecked;
     CComQIPtr<IFileDialogEvents> pEvents = cbk.GetUnknown();
 
-    if (m_CheckText[0] != 0)
+    if (!m_CheckText.IsEmpty())
     {
-        BROWSEINFO browseInfo       = {};
-        browseInfo.hwndOwner        = parent;
-        browseInfo.pidlRoot         = m_root;
-        browseInfo.pszDisplayName   = m_displayName;
-        browseInfo.lpszTitle        = m_title;
-        browseInfo.ulFlags          = m_style;
-        browseInfo.lParam           = (LPARAM)this;
-        browseInfo.lpfn             = BrowseCallBackProc;
+        CComPtr<IFileDialogCustomize> pfdCustomize;
+        if (FAILED(pfd.QueryInterface(&pfdCustomize)))
+            return CANCEL;
 
-        PCIDLIST_ABSOLUTE itemIDList = SHBrowseForFolder(&browseInfo);
+        pfdCustomize->StartVisualGroup(100, L"");
+        pfdCustomize->AddCheckButton(101, m_CheckText, FALSE);
+        if (!m_CheckText2.IsEmpty())
+            pfdCustomize->AddCheckButton(102, m_CheckText2, FALSE);
+        pfdCustomize->EndVisualGroup();
+    }
 
     DWORD eventsCookie;
     if (FAILED(pfd->Advise(pEvents, &eventsCookie)))
@@ -226,18 +151,26 @@ CBrowseFolder::retVal CBrowseFolder::Show(HWND parent, CString& path, const CStr
     if (FAILED(pfd->Show(parent)))
         return CANCEL;
 
-        if (ret != CANCEL)
-        {
-            if (!SHGetPathFromIDList(itemIDList, path.GetBuffer(MAX_PATH)))     // MAX_PATH ok. Explorer can't handle paths longer than MAX_PATH.
-                ret = NOPATH;
+    // Get the selection from the user
+    CComPtr<IShellItem> psiResult;
+    if (FAILED(pfd->GetResult(&psiResult)))
+        return CANCEL;
 
-            path.ReleaseBuffer();
-
-            CoTaskMemFree((LPVOID)itemIDList);
-        }
+    PWSTR pszPath = nullptr;
+    if (SUCCEEDED(psiResult->GetDisplayName(SIGDN_FILESYSPATH, &pszPath)))
+    {
+        path = pszPath;
+        CoTaskMemFree(pszPath);
     }
 
-    return ret;
+    CComPtr<IFileDialogCustomize> pfdCustomize;
+    if (SUCCEEDED(pfd.QueryInterface(&pfdCustomize)))
+    {
+        pfdCustomize->GetCheckButtonState(101, &m_bCheck);
+        pfdCustomize->GetCheckButtonState(102, &m_bCheck2);
+    }
+
+    return OK;
 }
 
 void CBrowseFolder::SetInfo(LPCTSTR title)
@@ -253,7 +186,7 @@ void CBrowseFolder::SetCheckBoxText(LPCTSTR checktext)
     ASSERT(checktext);
 
     if (checktext)
-        wcscpy_s(m_CheckText, checktext);
+        m_CheckText = checktext;
 }
 
 void CBrowseFolder::SetCheckBoxText2(LPCTSTR checktext)
@@ -261,184 +194,5 @@ void CBrowseFolder::SetCheckBoxText2(LPCTSTR checktext)
     ASSERT(checktext);
 
     if (checktext)
-        wcscpy_s(m_CheckText2, checktext);
+        m_CheckText2 = checktext;
 }
-
-void CBrowseFolder::SetFont(HWND hwnd,LPTSTR FontName,int FontSize)
-{
-
-    HFONT hf;
-    LOGFONT lf={0};
-    HDC hdc=GetDC(hwnd);
-
-    GetObject(GetWindowFont(hwnd),sizeof(lf),&lf);
-    lf.lfWeight = FW_REGULAR;
-    lf.lfHeight = (LONG)FontSize;
-    StringCchCopy( lf.lfFaceName, _countof(lf.lfFaceName), FontName );
-    hf=CreateFontIndirect(&lf);
-    SetBkMode(hdc,OPAQUE);
-    SendMessage(hwnd,WM_SETFONT,(WPARAM)hf,TRUE);
-    ReleaseDC(hwnd,hdc);
-
-}
-
-int CBrowseFolder::BrowseCallBackProc(HWND hwnd, UINT uMsg, LPARAM lParam, LPARAM /*lpData*/)
-{
-    //Initialization callback message
-    if (uMsg == BFFM_INITIALIZED)
-    {
-        if (m_CheckText[0] != 0)
-        {
-            bool bSecondCheckbox = (m_CheckText2[0] != 0);
-            //Rectangles for getting the positions
-            checkbox = CreateWindowEx(  0,
-                L"BUTTON",
-                m_CheckText,
-                WS_CHILD|WS_VISIBLE|WS_CLIPCHILDREN|BS_AUTOCHECKBOX,
-                0,100,100,50,
-                hwnd,
-                0,
-                NULL,
-                NULL);
-            if (checkbox == NULL)
-                return 0;
-
-            if (bSecondCheckbox)
-            {
-                //Rectangles for getting the positions
-                checkbox2 = CreateWindowEx( 0,
-                    L"BUTTON",
-                    m_CheckText2,
-                    WS_CHILD|WS_VISIBLE|WS_CLIPCHILDREN|BS_AUTOCHECKBOX,
-                    0,100,100,50,
-                    hwnd,
-                    0,
-                    NULL,
-                    NULL);
-                if (checkbox2 == NULL)
-                    return 0;
-            }
-
-            ListView = FindWindowEx(hwnd,NULL,L"SysTreeView32",NULL);
-            if (ListView == NULL)
-                ListView = FindWindowEx(hwnd,NULL,L"SHBrowseForFolder ShellNameSpace Control",NULL);
-
-            if (ListView == NULL)
-                return 0;
-
-            //Gets the dimensions of the windows
-            const int controlHeight = ::GetSystemMetrics(SM_CYMENUCHECK) + 4;
-            RECT listViewRect;
-            GetWindowRect(ListView,&listViewRect);
-            POINT pt;
-            pt.x = listViewRect.left;
-            pt.y = listViewRect.top;
-            ScreenToClient(hwnd, &pt);
-            listViewRect.top = pt.y;
-            listViewRect.left = pt.x;
-            pt.x = listViewRect.right;
-            pt.y = listViewRect.bottom;
-            ScreenToClient(hwnd, &pt);
-            listViewRect.bottom = pt.y;
-            listViewRect.right = pt.x;
-            //Sets the list view controls dimensions
-            SetWindowPos(ListView,0,listViewRect.left,
-                bSecondCheckbox ? listViewRect.top+(2*controlHeight) : listViewRect.top+controlHeight,
-                (listViewRect.right-listViewRect.left),
-                bSecondCheckbox ? (listViewRect.bottom - listViewRect.top)-(2*controlHeight) : (listViewRect.bottom - listViewRect.top)-controlHeight,
-                SWP_NOZORDER);
-            //Sets the window positions of checkbox and dialog controls
-            SetWindowPos(checkbox,HWND_BOTTOM,listViewRect.left,
-                listViewRect.top,
-                (listViewRect.right-listViewRect.left),
-                controlHeight,
-                SWP_NOZORDER);
-            if (bSecondCheckbox)
-            {
-                SetWindowPos(checkbox2,HWND_BOTTOM,listViewRect.left,
-                    listViewRect.top+controlHeight,
-                    (listViewRect.right-listViewRect.left),
-                    controlHeight,
-                    SWP_NOZORDER);
-            }
-            HWND label = FindWindowEx(hwnd, NULL, L"STATIC", NULL);
-            if (label)
-            {
-                HFONT hFont = (HFONT)::SendMessage(label, WM_GETFONT, 0, 0);
-                LOGFONT lf = {0};
-                GetObject(hFont, sizeof(lf), &lf);
-                HFONT hf2 = CreateFontIndirect(&lf);
-                ::SendMessage(checkbox, WM_SETFONT, (WPARAM)hf2, TRUE);
-                if (bSecondCheckbox)
-                    ::SendMessage(checkbox2, WM_SETFONT, (WPARAM)hf2, TRUE);
-            }
-            else
-            {
-                //Sets the fonts of static controls
-                SetFont(checkbox,L"MS Sans Serif",12);
-                if (bSecondCheckbox)
-                    SetFont(checkbox2,L"MS Sans Serif",12);
-            }
-
-            // Subclass the checkbox control.
-            CBProc = (WNDPROC) SetWindowLongPtr(checkbox,GWLP_WNDPROC, (LONG_PTR) CheckBoxSubclassProc);
-            //Sets the checkbox to checked position
-            SendMessage(checkbox,BM_SETCHECK,(WPARAM)m_bCheck,0);
-            if (bSecondCheckbox)
-            {
-                CBProc = (WNDPROC) SetWindowLongPtr(checkbox2,GWLP_WNDPROC, (LONG_PTR) CheckBoxSubclassProc2);
-                SendMessage(checkbox2,BM_SETCHECK,(WPARAM)m_bCheck,0);
-            }
-            // send a resize message to the resized list view control. Otherwise it won't show
-            // up properly until the user resizes the window!
-            SendMessage(ListView, WM_SIZE, SIZE_RESTORED, MAKELONG(listViewRect.right-listViewRect.left, bSecondCheckbox ? (listViewRect.bottom - listViewRect.top)-40 : (listViewRect.bottom - listViewRect.top)-20));
-        }
-
-        // now set the default directory
-        SendMessage(hwnd, BFFM_SETSELECTION, TRUE, (LPARAM)(LPCTSTR)m_sDefaultPath);
-    }
-    if (uMsg == BFFM_SELCHANGED)
-    {
-        // Set the status window to the currently selected path.
-        TCHAR szDir[MAX_PATH] = { 0 };
-        if (SHGetPathFromIDList((PCIDLIST_ABSOLUTE)lParam, szDir))
-        {
-            SendMessage(hwnd,BFFM_SETSTATUSTEXT, 0, (LPARAM)szDir);
-        }
-        else
-            return BFFM_VALIDATEFAILED;
-    }
-    if (uMsg == BFFM_VALIDATEFAILED)
-        return 1; //DONT_DISMISS
-
-    return 0;
-}
-
-LRESULT CBrowseFolder::CheckBoxSubclassProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
-{
-    if (uMsg == WM_LBUTTONUP)
-    {
-        m_bCheck = (SendMessage(hwnd,BM_GETCHECK,0,0)==BST_UNCHECKED);
-        if (m_bCheck && m_DisableCheckbox2WhenCheckbox1IsChecked)
-        {
-            ::EnableWindow(checkbox2, !m_bCheck);
-        }
-        else
-            ::EnableWindow(checkbox2, true);
-    }
-
-    return CallWindowProc(CBProc, hwnd, uMsg,
-        wParam, lParam);
-}
-
-LRESULT CBrowseFolder::CheckBoxSubclassProc2(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
-{
-    if (uMsg == WM_LBUTTONUP)
-    {
-        m_bCheck2 = (SendMessage(hwnd,BM_GETCHECK,0,0)==BST_UNCHECKED);
-    }
-
-    return CallWindowProc(CBProc, hwnd, uMsg,
-        wParam, lParam);
-}
-

src/Utils/MiscUI/BrowseFolder.h
@@ -1,6 +1,6 @@
 // TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2008, 2010, 2013 - TortoiseSVN
+// Copyright (C) 2003-2008, 2010, 2013, 2016 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -20,7 +20,6 @@
 
 /**
  * \ingroup Utils
- * A simple wrapper class for the SHBrowseForFolder API on XP.
  * On Vista and later, the IFileDialog is used with the FOS_PICKFOLDERS flag.
  */
 class CBrowseFolder
@@ -67,22 +66,10 @@ public:
 
     static BOOL m_bCheck;       ///< state of the checkbox on closing the dialog
     static BOOL m_bCheck2;
-    TCHAR m_title[200];
 protected:
-    static void SetFont(HWND hwnd,LPTSTR FontName,int FontSize);
-
-    static int CALLBACK BrowseCallBackProc(HWND  hwnd,UINT  uMsg,LPARAM  lParam,LPARAM  lpData);
-    static LRESULT APIENTRY CheckBoxSubclassProc(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);
-    static LRESULT APIENTRY CheckBoxSubclassProc2(HWND hwnd,UINT uMsg,WPARAM wParam,LPARAM lParam);
-
-    static WNDPROC CBProc;
-    static HWND checkbox;
-    static HWND checkbox2;
-    static HWND ListView;
     static CString m_sDefaultPath;
-    TCHAR m_displayName[200];
-    PCIDLIST_ABSOLUTE m_root;
-    static TCHAR m_CheckText[200];
-    static TCHAR m_CheckText2[200];
-    static bool m_DisableCheckbox2WhenCheckbox1IsChecked;
+    TCHAR m_title[200];
+    CString m_CheckText;
+    CString m_CheckText2;
+    bool m_DisableCheckbox2WhenCheckbox1IsChecked;
 };